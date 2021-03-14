a237a345887fb806f7091ac8b585385ab0e4fbf8
Abigail <abigailthelin@workfront.com>
2021-03-13 18:07:29 -0700

    fixed port

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ var app = (module.exports = express());
 
 app.use(express.static(__dirname + "/dist"));
 
-var port = 3000
+var port = process.env.PORT || 3000
 
-app.get("/", function(req, res) {
+app.get("*", function(req, res) {
     res.send(path.join(__dirname, "./dist/index.html"));
 });
 
